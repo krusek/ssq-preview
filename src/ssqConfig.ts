d7b1f1aa2bfc639b71b1bac12a6dd3ee85310074
Korben Rusek <korben.rusek@gmail.com>
2023-04-06 14:16:04 -0700

    support children and error handling
    - support "children" element
    - error handling for "lead"
    - error handling for "references"

--- src/ssqConfig.ts
+++ src/ssqConfig.ts
@@ -1,12 +1,9 @@
 import { error } from "console"
 
-export function convertJson(json: any): String {
-    var doc = json['doc']
-
-    var html = '<h1>' + json['header'] + '</h1>'
-    html += '<h2>' + json['title'] + '</h2>'
-    for (var index in doc) {
-        var paragraph = doc[index]
+function process_children(items: Array<any>): string {
+    var html: string = ""
+    for (var index in items) {
+        var paragraph = items[index]
         var p = paragraph['text']
         var references = paragraph['references'] || []
         references.sort((a, b) => a.start > b.start ? -1 : 1)
@@ -21,21 +18,44 @@ export function convertJson(json: any): String {
             }
             html += '</ul>';
         }
+        var children: Array<any> = paragraph['children'] ?? []
+        html = html + process_children(children)
     }
+    return html;
+}
+
+export function convertJson(json: any): string {
+    var doc = json['doc']
+
+    var html = '<h1>' + json['header'] + '</h1>'
+    html += '<h2>' + json['title'] + '</h2>'
+    html += process_children(json['doc'])
     return html
 }
 
-function insert_tag(text: String, open: String, close: String, start, end): String {
+function insert_tag(text: string, open: string, close: string, start, end): string {
     return text.substring(0, start) + open + text.substring(start, end) + close + text.substring(end)
 }
 
-function do_lead(text: String, lead): String {
+function do_lead(text: string, lead): string {
     if (lead) return insert_tag(text, '<b>', '</b>', 0, lead)
     return text
 }
 
-function validate_references(references: Array<any>): Array<String> {
-    var errors: Array<String> = []
+function validate_paragraph(paragraph: any): Array<string> {
+    var errors: Array<string> = []
+    const lead = paragraph['lead'] ?? 0
+    const text: string = paragraph['text']
+    const length = text.length
+    if (lead > text.length) {
+        const error = `"lead" value too large. Should be no more than ${length}`
+        errors.push(error)
+    }
+    return errors;
+}
+
+function validate_references(references: Array<any>): Array<string> {
+    var errors: Array<string> = []
     for (var i = 0; i < references.length; i++) {
         const reference = references[i];
         if (!reference.book) {
@@ -64,7 +84,7 @@ function validate_references(references: Array<any>): Array<String> {
     return errors
 }
 
-function process_references(text, references): String {
+function process_references(text, references): string {
     for (var index2 in references) {
         var reference = references[index2]
         var link = link_text(reference)
@@ -73,7 +93,7 @@ function process_references(text, references): String {
     return text
 }
 
-function link_text(reference): String {
+function link_text(reference): string {
     var link = reference.book
     if (reference.c)
         link += " " + reference.c
